1314e1838823d5d432a4c60b9ad8ac3fd76409f2
LeonardoBrizolla <leonardobrizollars@gmail.com>
2022-01-29 16:20:55 -0300

    🚧 Create IconSearch

---
+++ src/components/IconSearch/index.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import icon from '../../assets/images/icon-search.svg';
+
+const IconSearch = styled.img.attrs({ src: icon, alt: 'Ícone de busca' })``;
+
+export default IconSearch;
