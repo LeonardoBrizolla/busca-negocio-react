23b101c8d53e20e86e688eb053ddc1e619394300
LeonardoBrizolla <leonardobrizollars@gmail.com>
2022-01-29 22:27:18 -0300

    :lipstick: Update ServiceFilter style

--- src/components/ServiceFilter/styles.js
+++ src/components/ServiceFilter/styles.js
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
+import IconFilter from '../IconFilter';
 
 export const ServiceFilterStyle = styled.nav`
+  display: flex;
   background-color: #3a4042;
+
+  & > ${IconFilter} {
+    margin-right: 16px;
+  }
 `;
 
 export const Label = styled.p`
