169d586236c4ace7e72a7107ae75210c35781f73
Leonardo Brizolla <leonardobrizollars@gmail.com>
2022-02-07 11:05:09 -0300

    :sparkles: Create DescriptionBox component

---
+++ src/components/DescriptionBox/index.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const DescriptionBox = styled.p`
+  font-size: 20px;
+  letter-spacing: 0.15px;
+  color: #fffcee;
+  background-color: rgba(58, 64, 66, 0.5);
+  border-radius: 14px;
+  width: 288px;
+  padding: 10px;
+  line-height: 30px;
+`;
+
+export default DescriptionBox;
--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -3,6 +3,7 @@ import BackAction from '../../components/BackAction';
 import Stars from '../../components/Stars';
 import Title from '../../components/Title';
 import PhotoPreview from '../../components/PhotoPreview';
+import DescriptionBox from '../../components/DescriptionBox';
 
 function Details() {
   return (
@@ -12,6 +13,11 @@ function Details() {
       <Stars amount="5" />
       <Title>Cafeteria D Arte</Title>
       <PhotoPreview />
+      <DescriptionBox>
+        Aqui é só felicidade e café com um bom sol no verão e no inverno.
+        gostosas sopas para deixar o seu dia mais quente e feliz. <br />
+        Aqui o proprietário poderá escrever o que quiser sobre o seu café ;-)
+      </DescriptionBox>
     </>
   );
 }
