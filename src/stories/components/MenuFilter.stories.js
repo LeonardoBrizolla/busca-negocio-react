601b3ddc99f3003f0910854a0a0fa44fe16ee447
Leonardo Brizolla <leonardobrizollars@gmail.com>
2022-02-08 17:22:19 -0300

    :sparkles: Add props into MenuFilter Storybook

--- src/stories/components/MenuFilter.stories.js
+++ src/stories/components/MenuFilter.stories.js
@@ -1,5 +1,28 @@
 import MenuFilter from '../../components/MenuFilter';
 
+const items = [
+  { id: 'alimentacao', label: 'Alimentação' },
+  { id: 'animais', label: 'Animais' },
+  { id: 'arteecultura', label: 'Arte e Cultura' },
+  { id: 'associacoeseentidades', label: 'Associações / Entidades' },
+  { id: 'audioevideo', label: 'Áudio e Vídeo' },
+  { id: 'automoveis', label: 'Automóveis' },
+  { id: 'bancos', label: 'Bancos' },
+  { id: 'baresebebidas', label: 'Bares e Bebidas' },
+  { id: 'belezaeestetica', label: 'Beleza e Estética' },
+  { id: 'cafes', label: 'Cafés' },
+  { id: 'criancaebebe', label: 'Criança e Bebê' },
+  { id: 'diversaoelazer', label: 'Diversão e Lazer' },
+  { id: 'escritorioeseguranca', label: 'Escritório e Segurança' },
+  { id: 'esporteefitness', label: 'Esporte e Fitness' },
+  { id: 'fastfoods', label: 'Fast Foods' },
+  { id: 'festaseeventos', label: 'Festas e Eventos' },
+  { id: 'lojasecomercios', label: 'Lojas e comércios' },
+  { id: 'medicinaesaude', label: 'Medicina e Saúde' },
+  { id: 'tecnologiaeinformatica', label: 'Tecnologia e Informática' },
+  { id: 'utilidadepublica', label: 'Utilidade Pública' },
+];
+
 export default {
   title: 'components/MenuFilter',
   component: MenuFilter,
@@ -13,9 +36,11 @@ function Template(args) {
 export const Open = Template.bind({});
 Open.args = {
   active: true,
+  items,
 };
 
 export const Close = Template.bind({});
 Close.args = {
   active: false,
+  items,
 };
