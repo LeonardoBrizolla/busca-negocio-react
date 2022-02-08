d44f36747e74ff0a04f7e60a2d4ccefec5440f7f
Leonardo Brizolla <leonardobrizollars@gmail.com>
2022-02-08 15:07:47 -0300

    :sparkles: Centralize some Icons and Logo

--- src/stories/components/IconSearch.stories.js
+++ src/stories/components/IconSearch.stories.js
@@ -3,6 +3,9 @@ import IconSearch from '../../components/IconSearch';
 export default {
   title: 'components/IconSearch',
   component: IconSearch,
+  parameters: {
+    layout: 'centered',
+  },
 };
 
 export function Default() {
