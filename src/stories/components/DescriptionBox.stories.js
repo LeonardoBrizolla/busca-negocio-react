4821176d4ccbdc7b2a558a6bc25aacfba400de95
Leonardo Brizolla <leonardobrizollars@gmail.com>
2022-02-08 15:03:11 -0300

    :sparkles: Add props into DescriptionBox Storybook

--- src/stories/components/DescriptionBox.stories.js
+++ src/stories/components/DescriptionBox.stories.js
@@ -8,12 +8,13 @@ export default {
   },
 };
 
-export function Default() {
-  return (
-    <DescriptionBox>
-      Aqui é só felicidade e café com um bom sol no verão e no inverno. gostosas
-      sopas para deixar o seu dia mais quente e feliz. <br />
-      Aqui o proprietário poderá escrever o que quiser sobre o seu café ;-)
-    </DescriptionBox>
-  );
+function Template(args) {
+  return <DescriptionBox {...args} />;
 }
+
+export const Default = Template.bind({});
+
+Default.args = {
+  children:
+    'Aqui é só felicidade e café com um bom sol no verão e no inverno. gostosas sopas para deixar o seu dia mais quente e feliz. Aqui o proprietário poderá escrever o que quiser sobre o seu café ;-)',
+};
